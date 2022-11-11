069ad0434d3eb2b87c198b6b89d8575086468d7e
Vasili <kroxin1000@mail.ru>
2022-11-11 21:06:55 +0600

    ab955

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-проверка чтото поменял 
+let incr = 10,
+    decr = 10;
+
+
+   // incr++;
+   // decr--;
+
+   
+
+    console.log(++incr);
+    console.log(--decr);
+
+    console.log(5%2);
+
+
+    console.log(2*4 === '5');
+
+
+    const inchrkrt = true,
+        incolose = false;
+
+        console.log(inchrkrt || incolose);
