97186b4ed478bc06e36d01adc099f1e49966248b
Vasili <kroxin1000@mail.ru>
2022-11-12 14:52:13 +0600

    a1

--- pril/script.js
+++ pril/script.js
@@ -1,1 +1,20 @@
-dddd
+const numberOfil = promt('Сколько фильмов вы уже просмотрели?', '');
+
+const personalMoviDB = {
+
+    count:numberOfil,
+    movis: {},
+    actors: {},
+    geners: [],
+    prive: false
+};
+
+
+const a = prompt('Один из последних просмотреных фильмов', ''),
+      b = prompt('Насколько оцениваете?', ''),
+      c = prompt('Один из последних просмотреных фильмов', ''),
+      d = prompt('Насколько оцениваете?', '');
+
+
+personalMoviDB.movis[a]=b;
+personalMoviDB.movis[c]=d;
